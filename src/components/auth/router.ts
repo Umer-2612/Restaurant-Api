9c07a3977c29254cf15bde10fe7ecc724024d0a9
Neel <neelrana111@gmail.com>
2024-10-02 09:18:18 +0530

    NR - adding private route

--- src/components/auth/router.ts
+++ src/components/auth/router.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import AuthController from "./controller";
+import authenticate  from "../../config/middleware/auth";
 
 const router = Router();
 
 router.post("/sign-up", AuthController.signUp);
 
 router.post("/sign-in", AuthController.signIn);
+
+router.post("/sign-out", AuthController.signOut);
+
+router.get("/", authenticate.authenticate, AuthController.test);
 
 export default router;

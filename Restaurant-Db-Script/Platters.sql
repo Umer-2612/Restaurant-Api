df414d4b4425faef9bb6f5e5e89e01358e854921
Neel <neelrana111@gmail.com>
2024-10-06 11:03:27 +0530

    adding db scripts

---
+++ Restaurant-Db-Script/Platters.sql
@@ -0,0 +1,24 @@
+db.menu.insertMany([
+  {
+    itemName: "Vegetable Platters",
+    itemDescription: "2 pieces of each- veggie samosa, veggie pakora, mushroom pakora, onion bhaji.",
+    itemPrice: 21.90,
+    category: ObjectId("66fd6668ef4617bdbac4603a"),
+    recordDeleted: false,
+    createdBy: null,
+    updatedBy: null,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    itemName: "Tandoori Platters",
+    itemDescription: "2 pieces of each- Chicken tikka, tandoori chicken, lamb sheesh kebab, chicken pakora.",
+    itemPrice: 24.90,
+    category: ObjectId("66fd6668ef4617bdbac4603a"),
+    recordDeleted: false,
+    createdBy: null,
+    updatedBy: null,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }
+]);
---
+++ Restaurant-Db-Script/Vegan Curries.sql
@@ -0,0 +1,35 @@
+db.menu.insertMany([
+  {
+    itemName: "Vegan Butter Chicken (GF) (DF)",
+    itemDescription: "(Soy chunks or potatoes) plum tomato, coconut cream, fenugreek.",
+    itemPrice: 19.99,
+    category: ObjectId("66fd6668ef4617bdbac4603e"),
+    recordDeleted: false,
+    createdBy: null,
+    updatedBy: null,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    itemName: "Vegan Veg Korma (GF) (DF)",
+    itemDescription: "Mixed vegetables cooked in coconut creamy sauce with cashew and spices.",
+    itemPrice: 19.99,
+    category: ObjectId("66fd6668ef4617bdbac4603e"),
+    recordDeleted: false,
+    createdBy: null,
+    updatedBy: null,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    itemName: "Subzi",
+    itemDescription: "Your choice of meat cooked with ginger, coriander & fresh vegetables with medium spices.",
+    itemPrice: 19.99,
+    category: ObjectId("66fd6668ef4617bdbac4603e"),
+    recordDeleted: false,
+    createdBy: null,
+    updatedBy: null,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }
+]);

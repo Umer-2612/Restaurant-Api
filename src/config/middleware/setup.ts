d505be33402b05b60cb006d8f66bef9ca4b6923f
Umer Karachiwala <karachiwalaumer2612@gmail.com>
2025-01-10 00:00:17 +0530

    remove console logs

--- src/config/middleware/setup.ts
+++ src/config/middleware/setup.ts
@@ -14,7 +14,6 @@ import ThermalService from "../node-thermal-printer/service";
 export function setupMiddleware(app: Application): void {
   const corsOptions: CorsOptions = {
     origin: function (origin: string | undefined, callback: any) {
-      console.log({ aa: origin });
       if (!origin || Config.whiteList.indexOf(origin) !== -1) {
         // Allow requests from whitelisted origins or if there is no origin (e.g. Postman or server-side requests)
         callback(null, true);

5a97ffc4519481075a82f3f1f6fc90455f3b9305
Ayushya <ayushy52@gmail.com>
2025-01-12 14:38:49 +0530

    Fix: Updated the ip of the printing device

--- src/config/node-thermal-printer/configuration.ts
+++ src/config/node-thermal-printer/configuration.ts
@@ -3,7 +3,7 @@ const { ThermalPrinter, PrinterTypes } = require("node-thermal-printer");
 // Printer configuration
 const printer = new ThermalPrinter({
   type: PrinterTypes.EPSON, // Adjust to your printer model if needed
-  interface: "tcp://192.168.0.2", // Use 'usb' for USB-connected printers
+  interface: "tcp://192.168.29.2", // Use 'usb' for USB-connected printers
   options: {
     timeout: 5000, // Timeout for printer commands
   },

e3322259dea8fe4b46b6f05f7159f55351b4ca9f
Umer Karachiwala <karachiwalaumer2612@gmail.com>
2025-01-18 11:39:03 +0530

    fix: 1. comment mail functionality

--- src/components/reservation/utils.ts
+++ src/components/reservation/utils.ts
@@ -1,16 +1,15 @@
 import { IReservationRequestSchema } from "./interface";
-import {
-  reservationAcceptedTemplate,
-  reservationDeclinedTemplate,
-} from "../../utils/emailTemplates";
-import NodeMailerService from "../../config/node-mailer/service";
+import // reservationAcceptedTemplate,
+// reservationDeclinedTemplate,
+"../../utils/emailTemplates";
+// import NodeMailerService from "../../config/node-mailer/service";
 
 class ReservationRequestFormUtils {
-  private nodeMailerService: NodeMailerService;
+  // private nodeMailerService: NodeMailerService;
 
-  constructor() {
-    this.nodeMailerService = new NodeMailerService();
-  }
+  // constructor() {
+  //   this.nodeMailerService = new NodeMailerService();
+  // }
 
   public async sendReservationStatusUpdateEmail({
     oldReservationData,
@@ -25,30 +24,28 @@ class ReservationRequestFormUtils {
     ) {
       if (updatedReservationData.status === "Accepted") {
         // Send Mail to User about request accepted
-        const emailContent = await reservationAcceptedTemplate({
-          firstName: updatedReservationData.firstName,
-          lastName: updatedReservationData.lastName,
-          reservationDate: new Date(updatedReservationData.reservationDate),
-        });
-
+        // const emailContent = await reservationAcceptedTemplate({
+        //   firstName: updatedReservationData.firstName,
+        //   lastName: updatedReservationData.lastName,
+        //   reservationDate: new Date(updatedReservationData.reservationDate),
+        // });
         // Send email to the user about the request being accepted
-        await this.nodeMailerService.sendMail(
-          updatedReservationData.email,
-          emailContent
-        );
+        // await this.nodeMailerService.sendMail(
+        //   updatedReservationData.email,
+        //   emailContent
+        // );
       } else if (updatedReservationData.status === "Rejected") {
         // Send Mail to User about request rejected
-        const emailContent = await reservationDeclinedTemplate({
-          firstName: updatedReservationData.firstName,
-          lastName: updatedReservationData.lastName,
-          reservationDate: new Date(updatedReservationData.reservationDate),
-        });
-
+        // const emailContent = await reservationDeclinedTemplate({
+        //   firstName: updatedReservationData.firstName,
+        //   lastName: updatedReservationData.lastName,
+        //   reservationDate: new Date(updatedReservationData.reservationDate),
+        // });
         // Send email to the user about the request being accepted
-        await this.nodeMailerService.sendMail(
-          updatedReservationData.email,
-          emailContent
-        );
+        // await this.nodeMailerService.sendMail(
+        //   updatedReservationData.email,
+        //   emailContent
+        // );
       }
     }
   }
